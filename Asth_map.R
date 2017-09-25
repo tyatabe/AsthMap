3c19fc8d184ee0980adcd1693c6630e898778201
tyatabe <tadaishi@gmail.com>
2017-09-25 12:26:27 -0700

    has a map, need to rearrange still

--- Asth_map.R
+++ Asth_map.R
@@ -1,13 +1,14 @@
 # Load packages
 library(shiny)
-
+library(leaflet)
 # Load data
-pred <- readRDS("/home/tyatabe/Onedrive/Dev folder/AsthMap/data/pred.rds")
-
+#pred <- readRDS("/home/tyatabe/Onedrive/Dev folder/AsthMap/data/pred.rds")
+#ca_preds <- readRDS("/home/tyatabe/Onedrive/Dev folder/AsthMap/data/ca_preds.rds")
 
 # App of predicted rates asthma attack emergency dept visits  
 
 ui <- fluidPage(
+  # Leaflet output
   
   # App title ----
   titlePanel(h2("RiskMap")),
@@ -25,12 +26,15 @@ ui <- fluidPage(
       # Zip code
       textInput("zip.text", label = h3("Enter a zipcode"), value = "95616"),
       
+      
+      
       # Input: Slider for the number of bins ----
       sliderInput(inputId = "bins",
                   label = "Number of bins:",
                   min = 5,
                   max = 100,
                   value = 50)
+      
       
     ),
     
@@ -39,6 +43,8 @@ ui <- fluidPage(
       
       # Output selected zipcode
       textOutput("zipcode"),
+      # Tha map
+      leafletOutput("mymap"),
       # Output: Histogram ----
       plotOutput(outputId = "distPlot")
       
@@ -50,9 +56,26 @@ ui <- fluidPage(
 server <- function(input, output) {
   
   output$zipcode <- renderText({ 
-    paste("The predicted asthma attack rate per 10,000 at", input$zip.text, "is", 
+    paste("The predicted asthma attack rate per 10,000 at ", input$zip.text, " is ", 
           round(pred$pred[pred$zip.text==input$zip.text]), 
-          "the mean predicted rate for california is", round(mean(pred$pred)))
+          ". This is ", round(pred$pred[pred$zip.text==input$zip.text]
+            /mean(pred$pred)*100), "% of the mean rate of the state", sep="")
+  })
+  
+  output$mymap <- renderLeaflet({
+    leaflet() %>%
+      addProviderTiles("CartoDB.Positron") %>%
+      addPolygons(data = ca_preds, 
+                  fillColor = ~pal(pred), 
+                  color = "#b2aeae", # you need to use hex colors
+                  fillOpacity = 0.7, 
+                  weight = 1, 
+                  smoothFactor = 0.2,
+                  popup = popup) %>%
+      addLegend(pal = pal, 
+                values = ca_preds$pred, 
+                position = "bottomright", 
+                title = "Predicted asthma attack rates<br>per 10,000 people")
   })
   
   output$distPlot <- renderPlot({
@@ -65,6 +88,7 @@ server <- function(input, output) {
          main = "Asthma attacks ED visits rate per 10,000 in CA")
     
   })
+  
   
 }
 
