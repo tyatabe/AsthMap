308822d5e0e4b589d64fb212b0225208cb944fe5
tyatabe <tadaishi@gmail.com>
2017-09-26 22:44:10 -0700

    The best I've made

--- Asth_map.R
+++ Asth_map.R
@@ -2,9 +2,31 @@
 library(shiny)
 library(leaflet)
 # Load data
-#pred <- readRDS("/home/tyatabe/Onedrive/Dev folder/AsthMap/data/pred.rds")
-#ca_preds <- readRDS("/home/tyatabe/Onedrive/Dev folder/AsthMap/data/ca_preds.rds")
+pred <- readRDS("pred.rds")
+ca_preds <- readRDS("ca_preds.rds")
+popup <- paste0("Zip Code: ", ca_preds$zip, "<br>", 
+        "Predicted rate of ashtma attacks (per 10,000) is ", 
+        round(ca_preds$pred,1), ". This is ", 
+        round(ca_preds$pred/mean(ca_preds$pred, na.rm=T)*100, 1),
+        "% of the mean for CA")
+pal <- colorNumeric(
+  palette = "YlGnBu",
+  domain = ca_preds$pred
+)
 
+map3 <-leaflet() %>%
+  addProviderTiles("CartoDB.Positron") %>%
+  addPolygons(data = ca_preds, 
+              fillColor = ~pal(pred), 
+              color = "#b2aeae", # you need to use hex colors
+              fillOpacity = 0.7, 
+              weight = 1, 
+              smoothFactor = 0.2,
+              popup = popup) %>%
+  addLegend(pal = pal, 
+            values = ca_preds$pred, 
+            position = "bottomright", 
+            title = "Predicted asthma attack rates<br>per 10,000 people") 
 # App of predicted rates asthma attack emergency dept visits  
 
 ui <- fluidPage(
@@ -24,8 +46,8 @@ ui <- fluidPage(
                an asthma attack on any given year (CDC)"),
       
       # Zip code
-      textInput("zip.text", label = h3("Enter a zipcode"), value = "95616"),
-      
+      textInput("zip.text", label= h3("Enter a zipcode"), value = "95616"),
+      actionButton("get_risk", "Get Risk"),
       
       
       # Input: Slider for the number of bins ----
@@ -33,7 +55,9 @@ ui <- fluidPage(
                   label = "Number of bins:",
                   min = 5,
                   max = 100,
-                  value = 50)
+                  value = 50),
+      # Output: Histogram ----
+      plotOutput(outputId = "distPlot")
       
       
     ),
@@ -44,39 +68,42 @@ ui <- fluidPage(
       # Output selected zipcode
       textOutput("zipcode"),
       # Tha map
-      leafletOutput("mymap"),
-      # Output: Histogram ----
-      plotOutput(outputId = "distPlot")
+      leafletOutput("mymap")  
+      
       
       
     )
   )
+ 
 )
 
 server <- function(input, output) {
   
+ zip.input <-  eventReactive(input$get_risk, {
+    input$zip.text
+    
+  })
+  
+
+  
   output$zipcode <- renderText({ 
-    paste("The predicted asthma attack rate per 10,000 at ", input$zip.text, " is ", 
-          round(pred$pred[pred$zip.text==input$zip.text]), 
-          ". This is ", round(pred$pred[pred$zip.text==input$zip.text]
-            /mean(pred$pred)*100), "% of the mean rate of the state", sep="")
+    paste("The predicted asthma attack rate per 10,000 at ", zip.input(), " is ", 
+          round(pred$pred[pred$zip.text==zip.input()]), 
+          ". This is ", round(pred$pred[pred$zip.text==zip.input()]
+                              /mean(pred$pred)*100), "% of the mean rate of the state", sep="")
   })
   
   output$mymap <- renderLeaflet({
-    leaflet() %>%
-      addProviderTiles("CartoDB.Positron") %>%
-      addPolygons(data = ca_preds, 
-                  fillColor = ~pal(pred), 
-                  color = "#b2aeae", # you need to use hex colors
-                  fillOpacity = 0.7, 
-                  weight = 1, 
-                  smoothFactor = 0.2,
-                  popup = popup) %>%
-      addLegend(pal = pal, 
-                values = ca_preds$pred, 
-                position = "bottomright", 
-                title = "Predicted asthma attack rates<br>per 10,000 people")
-  })
+    map3 %>%
+    setView(lng = as.numeric(ca_preds@data$INTPTLON10[ca_preds@data$GEOID10==zip.input()]),
+            lat = as.numeric(ca_preds@data$INTPTLAT10[ca_preds@data$GEOID10==zip.input()]), 
+            zoom = 8) %>%
+    
+    addMarkers(lng=as.numeric(ca_preds@data$INTPTLON10[ca_preds@data$GEOID10==zip.input()]), 
+               lat=as.numeric(ca_preds@data$INTPTLAT10[ca_preds@data$GEOID10==zip.input()])
+    )
+    })
+  
   
   output$distPlot <- renderPlot({
     
@@ -85,7 +112,7 @@ server <- function(input, output) {
     
     hist(x, breaks = bins, col = "#75AADB", border = "white",
          xlab = "Rate per 10,000",
-         main = "Asthma attacks ED visits rate per 10,000 in CA")
+         main = paste("Distribution of asthma attacks rate in CA"))
     
   })
   
